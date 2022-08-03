022ff647b6c0af24db2812bd8bf8f2c5abbefe0b
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-03 21:45:59 +0900

    for문 활용) 문제 : 1부터 1000까지 출력

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-// 문제 : 구구구단 2단~9단을 출력해주세요.
-// 조건 : for문 사용
-// 출력예시
-/*
-2 * 1 = 2
-...
-9 * 9 = 81
-*/
+// 문제 : 1부터 1000까지 출력해주세요.
 console.clear();
 
-var dan;
-dan = 2;
-
-// 수정가능지역 시작 
-for (var dan = 2; dan <=9; dan++){
-    console.log("== " + dan + " 단 ==");
-    for ( var i = 1; i <= 9; i++ ) {
-    console.log(dan + " * " + i + " = " + dan * i);
-    }
+for(let i = 1; i<=1000; i++){
+    console.log(i);
 }
-
-// 수정가능지역 끝
