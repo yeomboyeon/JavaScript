13c193c09d1c255e2e5bfbb2e2f5e62435a18c37
yeomboyeon <yeomboyeon1221@gmail.com>
2022-08-03 15:43:32 +0900

    age의 값에 따라 성년,미성년 구분

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-// 문제 : `안녕하세요.`를 10번 출력해주세요.
-// 조건 : 변수를 사용할 수 없다.
+// 문제 : age의 값에 따라서 성년인지 미성년인지 출력해주세요
+console.clear();
+
+let age;
+
+console.log("== 정답 v1 ==");
+
+age = 20;
+
+if ( age >= 20 ) {
+  console.log("성년입니다.");
+}
+
+if ( age < 20 ) {
+  console.log("미성년입니다.");
+}
+
+console.log("== 정답 v2 ==");
+
+age = 15;
+
+if ( age >= 20 ) {
+  console.log("성년입니다.");
+}
+else {
+  console.log("미성년입니다.");
+}
+
+console.log("== 정답 v3 ==");
+
+age = 215;
+
+if ( age < 20 ) {
+  console.log("미성년입니다.");
+}
+else {
+  console.log("성년입니다.");
+}
