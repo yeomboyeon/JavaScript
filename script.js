34e6caae33455fe431c103062161f39fca3acba1
yeomboyeon <yeomboyeon1221@gmail.com>
2022-08-03 16:10:08 +0900

    중복 if문 활용(할인대상,비대상 구분)

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ console.log("당신의 나이는 " + age + "살 입니다.");
 // 구현시작
 // 출력예시 => 할인대상입니다. 또는 할인 대상이 아닙니다.
 
-if(age <= 19 || age >= 60){
+if (age <= 19) {
   console.log("할인 대상입니다.");
-}else {
-  console.log("할인 대상이 아닙니다.");
+}
+if (age >= 60) {
+  console.log("할인 대상입니다.");
+}
+if (age > 19) {
+  if (age > 60) {
+    console.log("할인 대상이 아닙니다.");
+  }
 }
