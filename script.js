45823b9ba455a61f9f63a00814d10ef9aea8b7cd
yeomboyeon <yeomboyeon1221@gmail.com>
2022-08-03 16:02:05 +0900

    if문 활용(할인대상,비대상 구분/&&)

--- script.js
+++ script.js
@@ -1,92 +1,18 @@
-// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
-// 조건 : 넘버링을 해주세요. EX : 참2, 참3, 거짓2, 거짓3 ...
+// 문제 : 할인 대상인지 아닌지 출력해주세요.
+// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
+// 조건 : 출력예시 처럼 출력되어야 합니다.
+// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
 
 console.clear();
+var age = 20; // 이 값을 바꿔가면서 실행해보세요.
 
-if ( true ) {
-  console.log("참");
-}
+console.log("당신의 나이는 " + age + "살 입니다.");
 
-if ( false ) {
-  console.log("거짓");
-}
+// 구현시작
+// 출력예시 => 할인대상입니다. 또는 할인 대상이 아닙니다.
 
-var a = 10;
-
-// `==` => 같다.
-if ( a == 10 ) {
-  console.log("참2");
-}
-
-// `!=` => 같지 않다.
-if ( a != 10 ) {
-  console.log("거짓2");
-}
-
-if ( a > 10 ) {
-  console.log("거짓3");
-}
-
-if ( a >= 10 ) {
-  console.log("참3");
-}
-
-var b = 10;
-
-if ( a == b ) {
-  console.log("참4");
-}
-
-var c = a != b;
-
-if ( c ) {
-  console.log("거짓4");
-}
-
-if ( c == false ) {
-  console.log("참5");
-}
-
-// `!` => 반전
-if ( !c ) {
-  console.log("참6");
-}
-
-// `!` => 반전
-if ( !(!c) ) {
-  console.log("거짓5");
-}
-
-var d = true;
-
-if ( c != d ) {
-  console.log("참7");
-}
-
-if ( c && d ) {
-  console.log("거짓6");
-}
-
-if ( c || d ) {
-  console.log("참8");
-}
-
-if ( c == false && d ) {
-  console.log("참9");
-}
-
-if ( c || c ) {
-  console.log("거짓7");
-}
-
-if ( false || false ) {
-  console.log("거짓8");
-}
-
-if ( 1 == 1 && 2 == 2 && 3 == 3 && 4 == 5 ) {
-  console.log("거짓9");
-}
-
-if ( 1 == 1 && 2 == 2 && 3 == 3 && 4 == 5 || true ) {
-  console.log("참10");
+if(19 <= age && age <= 60){
+  console.log("할인 대상입니다.");
+}else {
+  console.log("할인 대상이 아닙니다.");
 }
