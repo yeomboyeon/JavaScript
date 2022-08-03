6b459a62b85e693fafbfe89ed7ed47ffbb3af8d3
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-03 20:55:30 +0900

    while문 활용) 문제 : 구구단 2~9단 출력

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+// 문제 : 99단 2단~9단을 출력해주세요.
+// 조건 : 숫자를 2 이상 사용할 수 없다.
+// 조건 : while문 사용
+// 출력예시
+/*
+2 * 1 = 8
+...
+9 * 9 = 81
+*/
 console.clear();
 
-let dan = 8;
-let i = 1;
+var dan;
+var limit = 9;
+dan = 2;
 
+// 수정가능지역 시작
 
-while(i <= 9){
-  console.log(dan + " * " + i + " = " + dan * i);
-  i++;
+while (dan <= limit) {
+    /*2중 반복문 활용*/
+    console.log("=== " + dan + "단 ===");
+
+    var i = 1;
+
+    while (i <= limit) {
+        console.log(dan + " * " + i + " = " + dan * i);
+        i++;
+    }
+    dan++;
 }
+
+// 수정가능지역 끝
