3868b093272dd6a420d4c717cb1f53d89f2ce112
yeomboyeon <yeomboyeon1221@gmail.com>
2022-08-03 15:55:19 +0900

    참,거짓 구분

--- script.js
+++ script.js
@@ -1,38 +1,92 @@
-// 문제 : age의 값에 따라서 성년인지 미성년인지 출력해주세요
+// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
+// 조건 : 넘버링을 해주세요. EX : 참2, 참3, 거짓2, 거짓3 ...
+
 console.clear();
 
-let age;
-
-console.log("== 정답 v1 ==");
-
-age = 20;
-
-if ( age >= 20 ) {
-  console.log("성년입니다.");
+if ( true ) {
+  console.log("참");
 }
 
-if ( age < 20 ) {
-  console.log("미성년입니다.");
+if ( false ) {
+  console.log("거짓");
 }
 
-console.log("== 정답 v2 ==");
+var a = 10;
 
-age = 15;
-
-if ( age >= 20 ) {
-  console.log("성년입니다.");
-}
-else {
-  console.log("미성년입니다.");
+// `==` => 같다.
+if ( a == 10 ) {
+  console.log("참2");
 }
 
-console.log("== 정답 v3 ==");
-
-age = 215;
-
-if ( age < 20 ) {
-  console.log("미성년입니다.");
+// `!=` => 같지 않다.
+if ( a != 10 ) {
+  console.log("거짓2");
 }
-else {
-  console.log("성년입니다.");
+
+if ( a > 10 ) {
+  console.log("거짓3");
+}
+
+if ( a >= 10 ) {
+  console.log("참3");
+}
+
+var b = 10;
+
+if ( a == b ) {
+  console.log("참4");
+}
+
+var c = a != b;
+
+if ( c ) {
+  console.log("거짓4");
+}
+
+if ( c == false ) {
+  console.log("참5");
+}
+
+// `!` => 반전
+if ( !c ) {
+  console.log("참6");
+}
+
+// `!` => 반전
+if ( !(!c) ) {
+  console.log("거짓5");
+}
+
+var d = true;
+
+if ( c != d ) {
+  console.log("참7");
+}
+
+if ( c && d ) {
+  console.log("거짓6");
+}
+
+if ( c || d ) {
+  console.log("참8");
+}
+
+if ( c == false && d ) {
+  console.log("참9");
+}
+
+if ( c || c ) {
+  console.log("거짓7");
+}
+
+if ( false || false ) {
+  console.log("거짓8");
+}
+
+if ( 1 == 1 && 2 == 2 && 3 == 3 && 4 == 5 ) {
+  console.log("거짓9");
+}
+
+if ( 1 == 1 && 2 == 2 && 3 == 3 && 4 == 5 || true ) {
+  console.log("참10");
 }
