fd7d973ad9127a79c5383f6d67ff8f9478bbf145
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-08 20:35:12 +0900

    JS) 매번 다르게 행동하는 함수 구현(v2)

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 console.clear();
 
-// 문제 : 매번 다르게 행동하는 함수를 만들어주세요.
-let mode = 0;
+// 문제 : 매번 다르게 행동하는 함수(v2)를 만들어주세요.
 
-function hello() {
-    mode++;
+// var hello = function(mode) { } 와 아래는 같은 뜻
+function hello(mode) {
+    var msg = "안녕하세요.";
     
-    if(mode == 1){
-        console.log("안녕하세요");
-    } else if (mode == 2){
-        console.log("곤니찌와");
-    } else {
-        console.log("헬로");
+    if ( mode == "일본어" ) {
+        msg = "곤니찌와";
     }
+    else if ( mode == "영어" ) {
+        msg = "헬로";
+    }
+    
+    console.log(msg);
 }
 
-hello();
+// "한국어" => 함수에게 넘기는 부가적인 정보, 인자
+hello("한국어");
 // 출력 => 안녕하세요.
 
-hello();
+hello("일본어");
 // 출력 => 곤니짜와
 
-hello();
+hello("영어");
 // 출력 => 헬로
