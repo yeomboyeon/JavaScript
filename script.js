732861c85fa533bf321b0ed333ff152d0e2106f2
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-08 21:36:13 +0900

    JS) 4칙 연산 함수 구현

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// 함수는 자판기이다.
-// 함수에 입력값(동전과 음료수 버튼 번호)를 넣으면 반환값(음료수) 를 얻을 수 있다.
-// typeof 함수는 인자로 입력받은 값의 타입을 반환(리턴)한다.
-
+// 문제 : 4칙연산 함수를 만들고 실행해주세요.
+// 각각 다른 연산 함수를 입력하기 위해서는 함수명,변수명이 달라야 한다.
 console.clear();
 
-let a = 1 > 1;
-let typeOfA = typeof(a);
-console.log("typeOfA : " + typeOfA); // boolean
+function mode1(a, b){
+    let num1 = a + b;
+    return num1;
+};
 
-let b = 1 == 1;
-let typeOfB = typeof(b);
-console.log("typeOfB : " + typeOfB); // boolean
+function mode2(a, b){
+    let num2 = a - b;
+    return num2;
+};
 
-let c = 1 + 1;
-let typeOfC = typeof(c);
-console.log("typeOfC : " + typeOfC); // number
+function mode3(a, b){
+    let num3 = a * b;
+    return num3;
+};
 
-let d = "1 + 1";
-let typeOfD = typeof(d);
-console.log("typeOfD : " + typeOfD); // string
+function mode4(a, b){
+    let num4 = a / b;
+    return num4;
+};
+
+let num1 = mode1(1, 2);
+console.log(num1);
+
+let num2 = mode2(3, 2);
+console.log(num2);
+
+let num3 = mode3(1, 2);
+console.log(num3);
+
+let num4 = mode4(1, 2);
+console.log(num4);
