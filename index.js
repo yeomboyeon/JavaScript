e435a7209e032e67eb8784781ef31173a776a287
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-12 21:14:39 +0900

    JQuery) 마우스 클릭시 좌우로 움직이는 박스 만들기(최종 이해 완료)
    어려웠던 점
    - 함수명 뒤에 클래스명에는 . 을 붙이지 않는다.
    - 함수명은 class 첫글자 대문자로.. addClass

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-// .eq() : 선택한 요소의 인덱스 번호에 해당하는 요소를 찾습니다.
-// $("선택자").eq("숫자");
+let a = 0;
+const lasta = $('.slides > div').length-1; // 인덱스 번호 선택
 
-// eq() 탐색(Traversing)
-// .eq() 메서드는 선택한 요소에 인덱스 번호에 해당하는 요소를 찾습니다.
-// .filter() 메서드는 선택된 요소에서 선택하거나 함수를 사용할 수 있습니다.
-// .first() 메서드는 선택된 요소에서 첫번째 자식 요소를 찾습니다.
-// .has() 메서드는 선택된 요소의 자식 요소에서 주어진 선택자를 찾습니다.
-// .is() 메서드는 선택된 요소 객체의 특징을 판별합니다.
-// .last() 메서드는 선택된 요소에서 마지막 번째 자식 요소를 찾습니다.
-
-
-let order = 0; // order 변수 값 0으로 설정
-const lastorder = $(".juno > div").length - 1; 
-// div 박스가 움직이기 위해서는 배열에 맞춘다.
-// last 값을 인덱스값과 맞추기 위해 length -1해줘야 함
-
-$(".kapa > button") // 버튼을 클릭시 삭제 추가되는 구현
-    .eq(0) // 인덱스 번호 0번 (eq는 선택한 요소의 인덱스 번호에 해당하는 요소를 찾는다.)
-    .click(function () {
-    if (order === 0) { // 만약 클릭하기 전에 order 값이 0이면 last 값은 동일하니 가만히 있다.
-        order = lastorder;
-    } else { // 아니라면 order 값은 1씩 감소 시킨다.(그럼 마이너스로 가는 것과 동일하게 박스는 좌로 움직인다.)
-        order--;
-    }
-
-    $(".juno > div").removeClass("action"); // action 클래스를 remove 삭제 시킨다.
-    $(".juno > div").eq(order).addClass("action");  // action 클래스를 선택한 인덱스가 order 값이라면 add 추가 시킨다.
-    })
-
-$(".kapa > button")
-    .eq(1)
-    .click(function () {
-    if (order === lastorder) {
-        order = 0;
+$('.btns > button').eq(0).on('click', function(){
+    if(a === 0){ // 1번칸에서 5번칸으로 가려면 lasta로 가야 한다.
+        a = lasta;
     } else {
-        order++;
+        a--; // 좌로 가야하니 감소시킨다.
     }
+    $('.slides > div').removeClass('active'); // 함수명 대문자 잘 쓰자..
+    $('.slides > div').eq(a).addClass('active'); // 함수명 뒤에 클래스명에는 . 을 쓰지 않는다.
+})
 
-    $(".juno > div").removeClass("action");
-    $(".juno > div").eq(order).addClass("action");
-    })
-// 좌, 우 버튼을 각각 누르면 박스 위치가 변경되도록 구현
+$('.btns > button').eq(1).on('click', function(){
+    if(a == lasta){ // 5번칸에서 1번칸으로 가려면 인덱스번호 0이 되야 간다.
+        a = 0; 
+    }else {
+        a++; // 우로 가야하니 증가시킨다.
+    }
+    $('.slides > div').removeClass('active'); // 삭제
+    $('.slides > div').eq(a).addClass('active'); // 선택한 a 칸에 추가
+})
