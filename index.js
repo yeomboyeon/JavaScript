82fc4208ca80bc0ce966870d33080123eb6912f0
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-14 17:42:49 +0900

    ajex) 개념

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-$('#addClassBtn').click(function(){ // addClass 로 추가
-    $('#target').addClass('testClass');
-});
+/**
+ * - url : 요청 url을 의미한다.
+- type : 데이터 전송방식. GET 또는 POST
+- cache : 요청 페이지의 캐시 여부. false 또는 true
+- dataType : 서버에서 받아올 데이터를 어떤 형태로 해석할 것인지. xml, json, html, script를 선택할 수 있다.
+- data : 서버로 데이터를 전송할 때 사용한다. "name="+name 이런 형태로
+- success : Ajax 통신에 성공했을 때 실행되는 이벤트
+- error : Ajax 통신에 실패했을 때 실행되는 이벤트. request, status, error로 에러 정보를 확인할 수 있다.
+ */
 
-$('#removeClassBtn').click(function(){ // removeClass 로 삭제
-    $('#target').removeClass('testClass');
-});
+$.ajax({
+    url: "",
+    type: "",
+    cache: ,
+    dataType: "",
+    data: "",
+    success: function(data){
 
-$('#hasClassBtn').click(function(){ // hasClass 로 해당 태그 찾기
-    if($('#target').hasClass('testClass') == true) {
-    alert('textClass 있음');
-    } else if ($('#target').hasClass('testClass') == false) {
-    alert('textClass 없음');
+    },
+    error: function (request, status, error){        
+
     }
-});
-
-$('#toggleClassBtn').click(function(){ // 지정한 요소에 클래스 추가
-    $('#target').toggleClass('testClass');
 });
