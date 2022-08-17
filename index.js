b0f6f1a25f54b0ff575ecc35c30a9ed725e35b6e
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-17 17:55:50 +0900

    JQuery) html(), text(), width(), heigth()
    html() : 해당 요소의 HTML 컨텐츠를 반환하거나 설정함
    text() : 해당 요소의 텍스트 컨텐츠를 반환하거나 설정함
    width() : 선택한 요소 중에서 첫번째 요소의 너비를 픽셀 단위의 정수로 반환하거나 설정
    heigth() : 선택한 요소 중에서 첫번째 요소의 높이를 픽셀 단위의 정수로 반환하거나 설정

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
+'use strict';
+
 console.clear();
 
-$('.page-nav > div').click(function(event){ // 나자신 선택(target)
-    const target = $(event.target); // 타겟 설정
-    const myTarget = target.index(); // 타겟을 인덱스에 저장
+// // html(), text()
+// $('#btn1').on('click', function(){
+//     const msg = $('p').html();
+//     $('#output').html(msg);
+// })
 
-    //배경 사진 변경
-    $('.slider > .slides > div').removeClass('active'); 
-    // 사진이 있는 div에 active 태그 삭제
-    $('.slider > .slides > div').eq(myTarget).addClass('active'); 
-    // 사진이 있는 div에 선택한 mytarget에 active 태그 추가
-    
-    // 메뉴 활성화 변경
-    target.siblings().removeClass('active'); 
-    // siblings() : 선택한 요소의 형제 요소를 모두 찾아 active 태그를 삭제한다.
-    target.addClass('active'); // 선택한 요소에 active 태그를 추가한다.
+// $('#btn2').on('click', function(){
+//     const msg = $('p1').text();
+//     $('#output').html(msg);
+// })
+
+// width()
+// width : padding 안쪽 크기
+// innerWidth : border 안쪽 크기
+// outerWidth : border 포함 크기
+$('body').ready(function(){
+    const ybyWidth = $('p1').width();
+    const ybyinnerWidth = $('p1').innerWidth();
+    const ybyOuterWidth = $('p1').outerWidth();
+    $( 'p1' ).after( '<p>Width = ' + ybyWidth + ', Inner Width = ' + ybyinnerWidth + ', Outer Width = ' + ybyOuterWidth + '</p>' );
 })
 
-// 웹페이지의 특정 부분을 주기적으로 업데이트해줘야 하거나, 어떤 API로 부터 변경된 데이터를 주기적으로 받아와야 하는 경우
-// 어떤 코드를 일정한 시간 간격을 두고 반복해서 실행하고 싶을 때 사용
-// setTimeout() 함수와 대동소이. 첫번째 인자로 실행할 코드를 담고 있는 함수를 받고, 두번째 인자로 반복 주기를 밀리초(ms) 단위로 받음.
-setInterval(function(){
-    let changeIndex = $('.slides > .active').index()+1; // 변화를 캐치, 우로 1씩 증가
-
-    if($('.page-nav > div').length === changeIndex){ // 배열의 길이가 changeIndex 길이와 같다면
-        changeIndex = 0; // 인덱스 번호 0으로 돌아와라.
-    }
-
-    $('.slides > div').removeClass('active');
-    $('.slides > div').eq(changeIndex).addClass('active');
-
-    $('.page-nav > div').removeClass('active');
-    $('.page-nav > div').eq(changeIndex).addClass('active');
-}, 2000) // 밀리초(ms) 단위로 숫자 입력(2000이면 2초)
+// heigth()
+// height() : 내부높이
+// innerHeight() : 내부높이 + padding
+// outerHeight() : 내부높이 + padding + border
+// outerHeight(true) : 내부높이 + padding + border + margin
+$('body').ready(function(){
+    const ybyheigth = $('p2').heigth();
+    const ybyinnerheigth = $('p2').innerHeigth();
+    const ybyOuterheigth = $('p2').outerHeigth();
+    const ybyOuterheigthTrue = $('p2').outerHeigth(true);
+    $( 'p2' ).after( '<p>heigth = ' + ybyheigth + ', Inner heigth = ' + ybyinnerheigth + ', Outer heigth = ' + ybyOuterheigth + 'ybyOuterheigthTrue = ' + outerHeigth + '</p>' );
+})
