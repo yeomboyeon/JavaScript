3502621837561a8c4744921a47215e2c70fc99ee
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-19 21:01:17 +0900

    JQuery) 카카오톡 채팅방 만들기 구현(진행중_v1)
    처리 완료 : 엔터 및 전송 버튼을 클릭하면 입력한 값이 출력은 가능해짐.
    
    진행중
    - 출력되는 값의 위치, 모양 등이 함께 따라오질 않음.
    - 아들과 엄마의 대화가 티키타카가 되질 않음.
    - 계속 대화가 이어졌을 때 스크롤바가 내려가야 하는데..

--- index.js
+++ index.js
@@ -12,17 +12,19 @@ $('#input-form').on('submit', function(event){
     event.preventDefault();
   
     // 여기부터 구현
-  
-    // console.log('실행..');
-  })
-  
-  // 2. 내가 입력한 값을 가져온다 $('input').val()
-  
-  
-  // 3. 맨 아래로 입력한 값이 들어간다. append()
-  
-  
-  // 4. 내가 입력한 값은 지워진다. $('input').val('')
-  
-  
-  // 5. 역할 바꿔서 채팅방에서 써보도록 구현(라디오 박스 활용)
+
+    $(function() {
+        // 전송 버튼을 누르면 입력한 값이 출력된다.
+        $('.btn-submit').click(function() {
+            $('#text-input').addClass($('.message-group').eq(1).append($('input').val()));
+            $('#text-input').removeClass($('input').val(''));
+        });
+      
+        $(document).ready(function(){
+            $("#text-input").keydown(function(key){
+                if(key.keyCode==13){ //키의 코드가 13번일 경우 (13번은 엔터키)   
+                }
+            });
+        });
+    }); 
+})
