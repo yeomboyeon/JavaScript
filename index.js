2583482af06e2e96a9fd33cc22260f1844cbc7e1
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-31 21:19:32 +0900

    JQuery) addEvents, 입력todo, clear, 구현
    /**
     * ===== 입력한 todo 구현 (완료)
     * 입력한 todo 기록하기
     * 엔터 기능 구현하기
     * input에다가 form 태그로 감싸기  0
     * submit event로 받기 0
     * 새로고침 막기 event.preventDault() 0
     * 템플릿 리터널 활용
     * 입력값 가져오기 맨위로 나오도록 prepend
     * 입력창에 입력한 값 지우기
     *
     * ===== clear 버튼 구현 (완료)
     * clear 누르면 지금까지 출력된 거 없애기
     * clear 버튼에 event onclick 넣기
     * todo 없애기
     * 테스트 console.log("");
     *
     * ===== 단일 삭제 구현 (진행중)
     * 삭제 버튼 누르면 event 넣기
     * 삭제
     *
     * ===== 단일 수정 구현 (진행중)
     * 수정 버튼에 이벤트 넣기
     * input 으로 바꾸기전에 텍스트(엘리먼트.text()) 기억하기(변수에 저장)
     * input val(텍스트)로 기억
     * form 구조 변경(추가)
     */

--- index.js
+++ index.js
@@ -1,58 +1,52 @@
-/** 게시판 자료를 ajax를 사용하여 가져오기
- * 
- * =====1단계 (구현 완료)
- * 1. 데이터를 어디서 가져올지 확인
- * 2. document 요청 데이터 가져오기
- * 3. 데이터를 보여줄 화면을 선택자로 가져오기
- * 4. 00개의 데이터를 반복문을 통해서 html로 만들기
- * 5. 만든걸 배열로 저장
- * 6. append(html) 맨 아래로 보여주기
- * 
- * ======2단계 (구현 전)
- * 7. 데이터를 불러오는 중간에 로딩화면 구현
+/**
+ * ===== 입력한 todo 구현 (완료)
+ * 입력한 todo 기록하기
+ * 엔터 기능 구현하기
+ * input에다가 form 태그로 감싸기  0
+ * submit event로 받기 0
+ * 새로고침 막기 event.preventDault() 0
+ * 템플릿 리터널 활용
+ * 입력값 가져오기 맨위로 나오도록 prepend
+ * 입력창에 입력한 값 지우기
+ *
+ * ===== clear 버튼 구현 (완료)
+ * clear 누르면 지금까지 출력된 거 없애기
+ * clear 버튼에 event onclick 넣기
+ * todo 없애기
+ * 테스트 console.log("");
+ *
+ * ===== 단일 삭제 구현 (진행중)
+ * 삭제 버튼 누르면 event 넣기
+ * 삭제
+ *
+ * ===== 단일 수정 구현 (진행중)
+ * 수정 버튼에 이벤트 넣기
+ * input 으로 바꾸기전에 텍스트(엘리먼트.text()) 기억하기(변수에 저장)
+ * input val(텍스트)로 기억
+ * form 구조 변경(추가)
  */
 
-$('document').ready(function(){
-    // 한번 실행시켜주기 때문에 아래의 태그들은 한번만 실행하면 되는 것들을 추가
-    let html = '<table>'; 
-        html += '<thead>';
-        html += '<th>번호</th>';
-        html += '<th>글제목</th>';
-        html += '<th>내용</th>';
-        html += '</thead>';
-        html += '<tbody>';
+$(document).ready(function () {
+  addEvents();
+});
 
-    $.ajax({
-        url: "https://jsonplaceholder.typicode.com/posts",
-        async : false, // 비동기를 동기로 바꿔서 실행
-        success: function(response){
-            // console.log(response);
+function addEvents() {
+  $("#input-form").on("submit", function (event) {
+    event.preventDefault();
 
-            // 게시판에 데이터가 100개가 있는데 이거는 반복문을 통해서 추가를 하고 그들을 감싸는 태그도 반복되어 추가되도록 여기에 추가
-            for(let i = 0; i < response.length; i++){ // 불러오는 주소(response)에 있는 데이터만큼(.length) 반복
-                
-                const data = response[i]; // 데이터를 불러오는 주소의 데이터만큼 배열로 저장(response[i])
-                
-                // 아래의 태그들은 반복해서 실행해야만 계속 추가되어 출력됨
-                // 100개의 데이터를 선택자(${})로 불러오기 
-                // id, title, userId 변수명은 url 주소에서 검사했을 때 
-                // 데이터 구성 확인해보면 불러와야 하는 태그명들을 작성하였음.
-                html += '<tr>';
-                html += `<td>${data.id}</td>`; 
-                html += `<td>${data.title}</td>`;
-                html += `<td>${data.userId}</td>`;
-                html += '</tr>';
-            }
-        },
-        error: function (request, status, error){        
-        }
-    });
-    // 반복문 아래 닫아줘야 하는 태그들 추가
-    html += '</tbody>';
+    const message = $("input[type=text]").val();
 
-    html += '</table>';
+    let html = `<li><span> ${message} </span>
+              <button type="button">삭제</button>
+              <button type="button">수정</button></li>`;
 
-    // 출력되어야 하는 위치에 있는 선택자($()) 아래(append)에 불러오는 html을 넣어주기
-    $('.container').append(html);
+    $("form > ul").prepend(html);
 
-})
+    $("input[type=text]").val("");
+  });
+
+  $("#input-form > div > button").on("click", function () {
+    // empty(자식들 전체 삭제) 와 remove(자신포함 완전 삭제)
+    $("#input-modify-form > ul").empty();
+  });
+}
