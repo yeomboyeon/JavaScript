007807f12ce7bcd8f45fcf0b77493fb8cd72405a
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-09-14 21:47:03 +0900

    Js) Sperad operator, 최고, 최저값, 배열 합치기

--- index.js
+++ index.js
@@ -1,94 +1,49 @@
-/** 입력 기능 구현
- * document 에서 addEvents 함수 받아오기
- * addEvents 함수 선언
- * event 감싸기
- * 입력한 todo 기록 및 가져오기 input
- * input 태그에 form 태그로 감싸주기
- * on. submit. event.preventDault() 새로고침 막기
- */
-/** CLEAR 기능 구현
- * ul 태그 자식인 li 전부가 삭제되어야 함
- * remove는 자신만 삭제 empty 나포함 전체 삭제
- */
-/** 각 태그별 삭제 기능 구현
- * 이벤트 타겟 설정
- * 타겟 받아와서 변수에 저장
- * 클릭하는 해당 태그만 삭제
- * deleteEvents 함수 별도 선언
- * 삭제 remove()
- * onclick 함수
- */
-/** 각 태그별 수정 기능 구현
- * onclick html 태그에 작성
- * showDateEvents() 함수 선언
- * 수정 클릭시 해당 태그만 수정
- * 타겟 설정, 타겟의 형제태그를 변수에 저장
- * 그 변수의 텍스트를 또다른 변수에 저장
- * html 변수에 수정되는 값의 텍스트를 저장(input value)
- * 타겟의 형제의 첫번째 태그 이름을 prop 찾아서 변수에 저장
- * if 태그이름이 input 이면 리턴
- * 타겟의 형제 태그 변수의 부모의 첫번째에 html 추가
- * 타겟의 형제 태그 변수를 지운다.
- * ---addEvents--- 추가
- * submit 새로고침 막고
-입력한 값 어딘지 find 찾고
-찾은 값 val 입력받아
-부모, 첫번째 스팬태그에 입력 재입력하고
-기존에 입력된 값 remove 지운다.
- */
-$(document).ready(function () {
-  addEvents();
-});
+"use strict";
 
-function deleteEvents(event) {
-  const target = $(event.target);
-  target.parent().remove();
+console.clear();
+
+/** 1. 최고, 최저값 찾기 (Sperad operator 이용)
+ * Math.max(), Math.min()  */
+const array1 = [1, 2, 3, 4, 5];
+const c = [...array1]; // 배열 문자열로 벗끼기
+
+console.log(...array1); // 1 2 3 4 5
+
+console.log(c); // (5) [1, 2, 3, 4, 5]
+
+const d = {
+  name: "보연",
+};
+const f = { ...d }; // 객체 감싸기
+
+console.log(f); // {name: '보연'}
+
+const names = "보연";
+
+console.log(...names); // 보 연
+
+function solutionMax(params) {
+  return Math.max(...params);
 }
 
-function showDateEvents(event) {
-  const target = $(event.target);
-  const todo = target.siblings("span");
-  const todoText = todo.text();
-  const html = `<input type ="text" value ="${todoText}"/>`;
-  const nowTagName = target.siblings().first().prop("TagName");
+console.log("최고값 :", solutionMax(array1)); // 최고값 : 5
 
-  if (nowTagName === "INPUT") {
-    return;
-  }
-  target.parent().prepend(html);
-
-  todo.remove();
+function solutionMin(params) {
+  return Math.min(...params);
 }
+console.log("최저값 :", solutionMin(array1)); // 최저값 : 1
 
-function addEvents() {
-  $("#input-form").on("submit", function (event) {
-    event.preventDefault();
+// 배열 합치기
+// (기존 버전)
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
 
-    const todo = $("#input-form > input[type=text]").val();
+// const arr = arr1.concat(arr2);
+// console.log(arr); // (6) [1, 2, 3, 4, 5, 6]
 
-    let html = `<li>
-    <span> ${todo} </span>
-    <button type="button" onclick="deleteEvents(event)"/>삭제</button>
-    <button type="button" onclick="showDateEvents(event)"/>수정</button></li>`;
+// (ES6 버전)
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
 
-    $("#input-modify-form > ul").prepend(html);
-
-    $("#input-form > input[type=text]").val("");
-  });
-
-  $("#input-form > div > button").on("click", function () {
-    $("#input-modify-form > ul > li").empty();
-  });
-
-  $("#input-modify-form").on("submit", function (event) {
-    event.preventDefault(); // submit 새로고침 막고
-
-    const updateInput = $("#input-modify-form").find("input[type=text]"); // 입력한 값 어딘지 find 찾고
-
-    const updateTodo = updateInput.val(); // 찾은 값 val 입력받아
-
-    updateInput.parent().prepend(`<span>${updateTodo}</span>`); // 부모, 첫번째 스팬태그에 입력 재입력하고
-
-    updateInput.remove(); // 기존에 입력된 값 remove 지운다.
-  });
-}
+const arr = [...arr1, ...arr2];
+console.log(arr); // (6) [1, 2, 3, 4, 5, 6]
