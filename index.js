4a8c8c93130e900f14989dd0791d4a0a1e08d061
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-09-19 19:17:25 +0900

    JS) 최고값, 최소값 구하기_2

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 "use strict";
 
 console.clear();
-
-const a = [100, 200, 1, 2, 500, 214];
+// ticket 제일 많이, 적게 가진 인원의 ticket 갯수는?
+const user = [
+  {
+    name: "보연",
+    ticket: 38,
+  },
+  {
+    name: "연정",
+    ticket: 37,
+  },
+  {
+    name: "세희",
+    ticket: 4,
+  },
+  {
+    name: "관우",
+    ticket: 2,
+  },
+];
 
 let maxNumber = Number.MIN_SAFE_INTEGER;
 let minNumber = Number.MAX_SAFE_INTEGER;
 
-console.log(maxNumber); // -9007199254740991
-console.log(minNumber); // 9007199254740991
+let ticketMaxNumber = maxNumber;
+let ticketMinNumber = minNumber;
 
-// max값이 배열을 돌면서 가장 높은 값이 남고 그 값은 maxNumber 값이 된다.
-// min값이 배열을 돌면서 가장 낮은 값이 남고 그 값은 minNumber 값이 된다.
-for (let k = 0; k < a.length; k++) {
-  if (maxNumber < a[k]) {
-    maxNumber = a[k];
+for (let k = 0; k < user.length; k++) {
+  if (maxNumber < user[k].ticket) {
+    maxNumber = user[k].ticket;
+    ticketMaxNumber = user[k].ticket;
   }
-  if (minNumber > a[k]) {
-    minNumber = a[k];
+  if (minNumber > user[k].ticket) {
+    minNumber = user[k].ticket;
+    ticketMinNumber = user[k].ticket;
   }
 }
-
-console.log(`최고값은 ? ${maxNumber}`); // 최고값은 ? 500
-console.log(`최저값은 ? ${minNumber}`); // 최저값은 ? 1
+console.log(ticketMaxNumber); // 38
+console.log(ticketMinNumber); // 2
