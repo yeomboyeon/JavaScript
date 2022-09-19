0ddbce7a910c8cfd20d0e9302097b408219b989b
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-09-19 16:32:12 +0900

    JS) 최고값, 최저값 구하기(MAX, MIN_SAFE_INTEGER)

--- index.js
+++ index.js
@@ -2,46 +2,24 @@
 
 console.clear();
 
-// 배열 추가, 삭제
-const a = [1, 2, 3, 4];
+const a = [100, 200, 1, 2, 500, 214];
 
-a.push(5); // 배열 맨뒤에 추가
-console.log(a); // 1,2,3,4,5
+let maxNumber = Number.MIN_SAFE_INTEGER;
+let minNumber = Number.MAX_SAFE_INTEGER;
 
-a.unshift(0); // 배열 맨앞에 추가
-console.log(a); // 0,1,2,3,4,5
+console.log(maxNumber); // -9007199254740991
+console.log(minNumber); // 9007199254740991
 
-a.pop(); // 배열 맨뒤 삭제
-console.log(a); // 0,1,2,3,4
-
-a.shift(); // 배열 맨앞 삭제
-console.log(a); // 1,2,3,4
-
-// delete 삭제
-const man = {
-  name: "보연",
-  age: 20,
-};
-
-delete man.name;
-
-console.log(man); // {age: 20}
-
-// Template literals(템플릿 리터럴)
-const myName = "보연";
-console.log(`나의 이름은 ${myName}입니다`); // 나의 이름은 보연입니다
-
-// default value: 사전에 정의된 값
-// 디폴트 값 적용 순서 : 좌측에서 우측으로 적용
-
-function add(a, b = 10) {
-  // b = 10으로 사전에 값을 정의함
-  return a + b;
+// max값이 배열을 돌면서 가장 높은 값이 남고 그 값은 maxNumber 값이 된다.
+// min값이 배열을 돌면서 가장 낮은 값이 남고 그 값은 minNumber 값이 된다.
+for (let k = 0; k < a.length; k++) {
+  if (maxNumber < a[k]) {
+    maxNumber = a[k];
+  }
+  if (minNumber > a[k]) {
+    minNumber = a[k];
+  }
 }
 
-console.log(add(1));
-// 첫번째 인자 a를 1로 주었고, 뒤에는 값이 없기에
-// b는 그대로 10 해서 결과는 1 + 10 = 11 나옴
-
-const [one, two, five = 50] = [10, 20, 70];
-console.log(five); // 70 우측에 할당된 값이 있다면 사전 정의된 값은 무시
+console.log(`최고값은 ? ${maxNumber}`); // 최고값은 ? 500
+console.log(`최저값은 ? ${minNumber}`); // 최저값은 ? 1
