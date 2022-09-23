b86c4fabafa9102d87f065b6aa9392b72f7029a3
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-09-23 19:09:08 +0900

    JS) sort() 배열 오름차순 정렬

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
 
 console.clear();
 
-// reduce() 초기값 지정해줘야 에러 발생하지 않는다.
-// sum 값을 지정해줌으로써 arr[0] = null으로 값이 설정된다.
-// 적용방법 : array.reduce(function(누적값, 현재값, 인덱스, 배열){ return 누적값 + 현재값 }, 0);
-//           array.reduce((누적값, 현재값, 인덱스, 배열) => return 결과, 초기값);
-
-const arrResult = arr.reduce(function add(sum, currValue) {
-  return sum + currValue;
-}, 0);
-console.log(`arr 배열값 합계는 ? ${arrResult}`);
+const arrSort = arr.sort(function (a, b) {
+  return a - b;
+  // return b - a; (내림차순 정렬)
+});
+console.log(`오름차순 정렬된 arrSort 값 ? ${arrSort}`);
