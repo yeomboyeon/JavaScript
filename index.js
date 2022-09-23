9b7cd7aebc4c0c279b04b6ea1a2c6d69bfcd0a23
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-09-23 19:08:32 +0900

    JS) reduce() : 배열 값 합계 구하기

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 
 console.clear();
 
-// splice(start[, deleteCount[, item1[, item2[, ...]]]])
-// - start : 음수이면 끝에서부터 세기
-// - deleteCount : 배열에서 제거할 요소의 수
-// - item : 배열에 추가할 요소(지정하지 않으면 splice() 요소 제거만 수행)
+// reduce() 초기값 지정해줘야 에러 발생하지 않는다.
+// sum 값을 지정해줌으로써 arr[0] = null으로 값이 설정된다.
+// 적용방법 : array.reduce(function(누적값, 현재값, 인덱스, 배열){ return 누적값 + 현재값 }, 0);
+//           array.reduce((누적값, 현재값, 인덱스, 배열) => return 결과, 초기값);
 
-const fruits = ["수박", "바나나", "망고", "파인애플"];
-// fruits.splice(2, 1);
-// console.log(fruits); // ['수박', '바나나', '파인애플']
-// fruits.splice(2, 2);
-// console.log(fruits); // ['수박', '바나나']
-const removed = fruits.splice(0, 3); // 삭제된 배열 값을 변수로 저장
-console.log(removed); // ['수박', '바나나', '망고']
-console.log(fruits); // ['파인애플']
+const arrResult = arr.reduce(function add(sum, currValue) {
+  return sum + currValue;
+}, 0);
+console.log(`arr 배열값 합계는 ? ${arrResult}`);
