649e82065ede0e06aac6897ce63b81cd445c695e
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-01 21:03:25 +0900

    JS) 등수 구하기 알고리즘(Array.from, 2중 반복문)

--- index.js
+++ index.js
@@ -2,21 +2,34 @@
 
 console.clear();
 
-// 회문 문자열 찾기
-// Math.floor() 소수점 이하 버림(홀수 일때 적용)
-// 소문자던 대문자던 바꾸고 검색하기
+/** 등수 구하기 알고리즘
+ * 첫 줄에 n(3<=n<=1000) 입력
+ * 두 번째 줄에 국어점수 의미하는 n개의 정수 입력
+ * 같은 점수가 입력될 경우 높은 등수로 동일 처리
+ * 가장 높은 점수가 92점인데 92점이 3명 존재하면
+ * 1등이 3명이고 그 다음 학생은 4등이 된다.
+ * 입력 예제 : 5
+ * 87 89 92 100 76
+ * 출력 예제 : 1
+ * 4 3 2 1 5
+ */
 
-function solution(str) {
-  let answer = "YES";
-  str = str.toLowerCase();
-  let len = str.lenght;
-  for (let i = 0; i < Math.floor(len / 2); i++) {
-    //   for (let i = 0; i < len.length; i++) {
-    if (str[i] !== str[len - i - 1]) {
-      return "NO";
+function solution(arr) {
+  const len = arr.length; // 배열을 len 변수에 저장
+  const answer = Array.from({ length: len }, () => 1); //  배열 값 모두에게 1 등으로 저장 [1, 1, 1, 1, 1];
+  // const answer = [1, 1, 1, 1, 1];
+  // 입력 배열이 많게 되면 Array.from을 활용해서 작성하면 됨
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[i]) answer[i]++;
+      // 2중 for문 돌면서 인덱스 i 값보다 인덱스 j 값이 크다면 answer 인덱스 i 값을 1씩 늘리기
+      // arr[j] = arr[i] 라면 answer[i] 은 증가되지 않음, 그래야만 동일 점수가 동일 등수가 됨
     }
   }
   return answer;
 }
-let str = "gooG";
-console.log(solution(str));
+
+let arr = [87, 89, 92, 100, 76];
+
+console.log(`등수 구하기 값은? ${solution(arr)}`);
