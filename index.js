92f59a3c8e93a6faa5efd8f6dbe7b543451b3d98
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-04 02:16:16 +0900

    JS) map(), Math.max(), …활용 : 배열 안에 배열 중 가장 큰 값 출력

--- index.js
+++ index.js
@@ -2,42 +2,26 @@
 
 console.clear();
 
-// filter()
-// 매개변수로 아이디값을 받아 해당되는 아이디값만 삭제
-// 4번 미아 티켓 제거
-function test_04(paramId) {
-  const user = [
-    {
-      id: 1,
-      name: "철수",
-      ticket: 0,
-    },
-    {
-      id: 2,
-      name: "영희",
-      ticket: 3,
-    },
-    {
-      id: 3,
-      name: "미아",
-      ticket: 12,
-    },
-    {
-      id: 4,
-      name: "길동",
-      ticket: 5,
-    },
-  ];
-  const newUser = user.filter((value, index) => {
-    return value.id !== paramId; // 같지 않은 값만 담기
-  });
-  console.log(newUser);
-}
-test_04(4);
-/** 4번은 같기 때문에 담기질 않아서 삭제됨
- * (3) [{…}, {…}, {…}]
-0: {id: 1, name: '철수', ticket: 0}
-1: {id: 2, name: '영희', ticket: 3}
-2: {id: 3, name: '미아', ticket: 12}
-length: 3
+/** map(), Math.max() 활용
+ * 각 배열 안에서 최대값 출력하기
+ * 입력 예제 : [[4,5,1,3][13,27,18,26][32,35,37,39][1000,1001,857,1]]
+ * 출력 예제 : [5,27,39,1001]
  */
+
+const arr = [
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+];
+
+function solutionMax() {
+  const newUser = arr.map((value, index) => {
+    // 배열에서 바꿀 값을 찾는다.
+    return Math.max(...value); // 바꿀 값은 배열에서 가장 큰 값을 찾아서 변수에 저장
+  });
+  console.log(newUser); // 해당 변수를 출력
+  // console.log(Math.max(...value));
+}
+
+solutionMax(); //(4) [5, 27, 39, 1001]
