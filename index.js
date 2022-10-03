4ea420e30c83c7652c1d9367697d1361be3f6999
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-03 20:37:29 +0900

    JS) findIndex() : 배열의 요소 중 값을 찾아서 그 값만 변경

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 
 console.clear();
 
-/** map()
- * 배열의 요소들의 값을 변경해서 새로운 배열로 활용
- * 반복 처리 가능
- */
-
-function test_02() {
+// findIndex()
+// 배열의 요소 중 값을 찾아서 그 값만 변경
+// 매개변수로 아이디값을 받아 해당되는 아이디값만 티켓을 하나 제거
+// 3번 미아 티켓 제거
+function test_03(paramId) {
   const user = [
     {
       id: 1,
@@ -30,15 +29,25 @@ function test_02() {
       ticket: 5,
     },
   ];
-  const newUser = user.map((value, index) => {
-    if (value.ticket < 5) {
-      value.ticket += 3;
-    }
-    return value;
+  const findIndex = user.findIndex((value, index) => {
+    return value.id === paramId;
   });
-  console.log(newUser);
+  console.log(findIndex); // 2
+  console.log(user[findIndex]); // {id: 3, name: '미아', ticket: 12}
+
+  user[findIndex].ticket = user[findIndex].ticket - 1; // 티켓 1 감소
+
+  console.log(user);
+  /**(4) [{…}, {…}, {…}, {…}]
+  0: {id: 1, name: '철수', ticket: 0}
+  1: {id: 2, name: '영희', ticket: 3}
+  2: {id: 3, name: '미아', ticket: 11} >> 티켓 1 감소
+  3: {id: 4, name: '길동', ticket: 5}
+  length: 4
+   */
 }
-test_02();
+test_03(3); // id가 3인 인원의 값을 변경
+// test_03(4); // id가 4인 인원의 값을 변경시켜준다.
 /**
  * (4) [{…}, {…}, {…}, {…}]
 0: {id: 1, name: '철수', ticket: 3} >> 값 증가
