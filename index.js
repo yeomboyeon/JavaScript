368b3e677572a9282d884dc7923f3e574fc8c375
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-05 20:51:51 +0900

    JS) 문자열 출력(홀,짝)

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-// 보이는 학생
-function sol() {
-  let arr = [130, 135, 148, 140, 145, 150, 150, 153]; // 입력 값
-  let max = arr[0]; // 최대값 입력하도록 배열 변수 저장
-  let cnt = 1; // 카운트 1씩 증가값 변수 저장
+// 문자열 짝수일 때 가운데 두 글자, 홀수일 때 한 글자
+function sol_1(s) {
+  let answer = ""; // 빈값 저장
 
-  for (let i = 1; i < arr.length; i++) {
-    // 입력 값 만큼 반복
-    if (max < arr[i]) {
-      // 배열값이 최대값보다 큰 값을 확인
-      console.log(arr[i]); // 조건에 해당하는 값 확인
-      max = arr[i]; // 최대값에 해당 값 저장
-      cnt++; // 조건 부합된 값을 증가
-    }
+  if (s.length % 2 == 0) {
+    // 짝수 2로 나누었을 때 나머지가 0 이면
+    answer = s[s.length / 2 - 1] + s[s.length / 2]; //
+  } else {
+    // 홀수
+    answer = s[Math.floor(s.length / 2)]; // 소수점 버리기
   }
-  return cnt;
+  return answer;
 }
-
-console.log(sol()); // 5
+let s = "abcde";
+console.log(sol_1(s));
