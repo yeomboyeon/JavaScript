8cf7954db8485d4cfe7b10b0be33585f9fa9b36e
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-08 17:12:15 +0900

    JS) 배열 값 뒤집기(reverse)

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
 "use strict";
 
 console.clear();
+/**reverse() : 배열 값 뒤집기
+ */
 
-// 자릿수의 합
-// 문자열로 바꿔서 반복문 돌리기
-// 입력예제 : 7
-// 입력예제 : 128 460 603 40 521 137 123
-// 출력예제 : 137
+const arr = [1, 2, 3, 4, 5];
+const arr1 = [6, 7, 8, 9, 10];
 
-console.clear();
+const arr2 = arr.reverse();
 
-function solution() {
-  const arr = [128, 460, 603, 40, 521, 137, 123];
-
-  let max = Number.MIN_SAFE_INTEGER;
-
-  let maxIndex = 0;
-
-  arr.forEach((value, index) => {
-    const str = value.toString(); // 숫자를 문자로 바꾸기
-
-    let sum = 0;
-
-    for (let x of str) {
-      // for of 객체랑 문자 반복문 돌릴 때 자주쓰임
-      // console.log(x);
-      sum += Number(x);
-    }
-
-    if (sum >= max) {
-      max = sum;
-      maxIndex = index;
-    }
-  });
-  return arr[maxIndex];
-}
-
-console.log(solution());
+console.log(arr2); // [5, 4, 3, 2, 1]
