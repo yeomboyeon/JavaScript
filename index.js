2e393a1d00702418e60c9b2becf6ab6f78b7615a
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-08 17:12:48 +0900

    JS) 배열 값 합치기(concat)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 "use strict";
 
 console.clear();
-/**reverse() : 배열 값 뒤집기
+/**concat(), ...변수명 : 배열 값 합치기
  */
 
 const arr = [1, 2, 3, 4, 5];
 const arr1 = [6, 7, 8, 9, 10];
 
-const arr2 = arr.reverse();
+const arr2 = [...arr, ...arr1];
 
-console.log(arr2); // [5, 4, 3, 2, 1]
+const arr3 = arr.concat(arr1);
+
+console.log(arr2); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arr3); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
